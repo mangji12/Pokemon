559bcbeb92339295c2676fa48f23d07c456d323d
mangji12 <dhckddls12@naver.com>
2024-04-25 21:16:13 +0900

    Initial Commit

---
+++ Pokemon/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Pokemon
+//
+//  Created by 막북이 on 4/25/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
